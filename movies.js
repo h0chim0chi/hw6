978f67e6c1fbc64f86601a44147b871bb2f7c3ea
h0chim0chi <jessehleung@gmail.com>
2021-02-18 18:45:52 -0500

    final

--- movies.js
+++ movies.js
@@ -22,28 +22,27 @@ window.addEventListener('DOMContentLoaded', async function(event) {
     for (let i=0; i<movies.length; i++) {
       let MovieName = Movies[i].id
       let poster = movies[i].poster_path
-    
-    
   
+document.querySelector('.movies').insertAdjacentHTML('beforeend',` 
+  <div class="w-1/5 p-4 movie-${MovieName}">
+  <img src="https://image.tmdb.org/t/p/w500/${poster}" class="w-full">
+  <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
+    </div>
+      `)
   
-      document.querySelector('.movies').insertAdjacentHTML('beforeend',` 
-       <div class="w-1/5 p-4 movie-${MovieName}">
-        <img src="https://image.tmdb.org/t/p/w500/${poster}" class="w-full">
-        <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
-       </div>
-          `)
-  
-          let docRef = await db.collection('watched').doc(`${MovieName}`).get()
-          let movie_select = docRef.data()
+        let docRef = await db.collection('watched').doc(`${MovieName}`).get()
+        let movie_select = docRef.data()
       
-          if(movie_select){
-            let watchedmovie = document.querySelector(`.movie-${MovieName}`)
-            watchedmovie.classList.add('opacity-20')
-        }
-           let watchedindicator = document.querySelector(`.movie-${MovieName}`)
+        if(movie_select){
+          let watchedmovie = document.querySelector(`.movie-${MovieName}`)
+          watchedmovie.classList.add('opacity-20')
+      }
+          let watchedindicator = document.querySelector(`.movie-${MovieName}`)
           console.log(watchedindicator)
+          
           watchedindicator.addEventListener(`click`, async function(event){
           event.preventDefault()
+          
           let watchedmovie = document.querySelector(`.movie-${MovieName}`)
           watchedmovie.classList.add('opacity-20')
           await db.collection(`watched`).doc(`${MovieName}`).set({})
