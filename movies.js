81a81e9763a562dd53594e1d0c831d83f627e589
h0chim0chi <jessehleung@gmail.com>
2021-02-18 20:33:37 -0500

    final

--- movies.js
+++ movies.js
@@ -29,7 +29,7 @@ window.addEventListener('DOMContentLoaded', async function(event) {
   
 document.querySelector('.movies').insertAdjacentHTML('beforeend',` 
   <div class="w-1/5 p-4 movie-${movies.id} opacity-20">
-  <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" class="w-full">
+  <img src="https://image.tmdb.org/t/p/w500/${poster}" class="w-full">
   <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
     </div>
       `)
