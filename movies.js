e0b307c6c2385439b12a8096c7cf40fb7b6ce688
h0chim0chi <jessehleung@gmail.com>
2021-02-18 19:41:13 -0500

    final

--- movies.js
+++ movies.js
@@ -19,34 +19,48 @@ window.addEventListener('DOMContentLoaded', async function(event) {
     console.log(Movies)
   
     for (let i=0; i<movies.length; i++) {
-      let movie = movies[i].id
+      let movie = movies[i]
+      let reference = await datastore.collection('watched').doc('${movie.id}').get()
+      let watchedmovie = reference.data()
       let poster = movies[i].poster_path
+      if (watchedmovie) {
+        opacityfeature = 'opacity-20'
+      }
   
 document.querySelector('.movies').insertAdjacentHTML('beforeend',` 
-  <div class="w-1/5 p-4 movie-${movies.id}">
+  <div class="w-1/5 p-4 movie-${movies.id} ${opacityfeature}">
   <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" class="w-full">
   <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
     </div>
       `)
+
+document.querySelector(`.movie-${movie.id}`).addEventListener('click', async function(event) {
+  event.preventDefault()
+  let watchedindicator = document.querySelector(`.movie-${movie.id}`)
+  watchedindicator.classList.add('opacity-20')
+  await datastore.collection('watched').doc(`${movie.id}`).set({})
+      }) 
+    }
+  })
   
-        let reference = await datastore.collection('watched').doc(`${movie}`).get()
-        let movie_select = reference.data()
+ //       let reference = await datastore.collection('watched').doc(`${movie}`).get()
+ //       let movie_select = reference.data()
       
-        if(movie_select){
-          let watchedmovie = document.querySelector(`.movie-${movie}`)
-          watchedmovie.classList.add('opacity-20')
+ //       if(movie_select){
+ //         let watchedmovie = document.querySelector(`.movie-${movie}`)
+ //         watchedmovie.classList.add('opacity-20')
       }
-          let watchedindicator = document.querySelector(`.movie-${movie}`)
-          console.log(watchedindicator)
+ //         let watchedindicator = document.querySelector(`.movie-${movie}`)
+ //         console.log(watchedindicator)
           
-          watchedindicator.addEventListener(`click`, async function(event){
-          event.preventDefault()
+ //         watchedindicator.addEventListener(`click`, async function(event){
+  //        event.preventDefault()
           
-          let watchedmovie = document.querySelector(`.movie-${movie}`)
-          watchedmovie.classList.add('opacity-20')
-          await datastore.collection(`watched`).doc(`${movie}`).set({})
+  //        let watchedmovie = document.querySelector(`.movie-${movie}`)
+   //       watchedmovie.classList.add('opacity-20')
+    //      await datastore.collection(`watched`).doc(`${movie}`).set({})
         
-        })
-      }
+  //      })
+    //  }
     
-   })
+  // })
