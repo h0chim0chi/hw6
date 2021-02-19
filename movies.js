b6d48c34b3f802d3bd3a4a175cb2481f21b6c87f
h0chim0chi <jessehleung@gmail.com>
2021-02-18 20:56:58 -0500

    final

--- movies.js
+++ movies.js
@@ -22,13 +22,9 @@ window.addEventListener('DOMContentLoaded', async function(event) {
       let movie = movielist[i]
       let reference = await datastore.collection('watched').doc(`${movie.id}`).get()
       let watchedmovie = reference.data()
-      let opacityfeature = ''
-      if (watchedmovie) {
-        opacityfeature = 'opacity-20'
-      }
   
 document.querySelector('.movies').insertAdjacentHTML('beforeend',` 
-  <div class="w-1/5 p-4 movie-${movie.id} ${opacityfeature}">
+  <div class="w-1/5 p-4 movie-${movie.id} opacity-20>
   <img src="https://image.tmdb.org/t/p/w500/${movie.poster_path}" class="w-full">
   <a href="#" class="watched-button block text-center text-white bg-green-500 mt-4 px-4 py-2 rounded">I've watched this!</a>
     </div>
